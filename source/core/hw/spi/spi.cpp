8b516ed4b9509c7d473615050c901e8fc235f9e1
Frederic Meyer <fleroviux@gmail.com>
2020-11-08 16:24:14 +0100

    Core\SPI: decode SPICNT writes.

--- source/core/hw/spi/spi.cpp
+++ source/core/hw/spi/spi.cpp
@@ -21,19 +21,30 @@ void SPI::Reset() {
 auto SPI::SPICNT::ReadByte(uint offset) -> u8 {
   switch (offset) {
     case 0:
-      return 0;
+      return baudrate | (busy ? 128 : 0);
     case 1:
-      return 0;
+      return static_cast<u8>(device) |
+            (bugged_hword_mode ?   4 : 0) |
+            (chipselect_hold   ?   8 : 0) |
+            (enable_irq        ?  64 : 0) |
+            (enable            ? 128 : 0);
   }
 
   UNREACHABLE;
 }
 
 void SPI::SPICNT::WriteByte(uint offset, u8 value) {
+  // TODO: verify that baudrate matches the selected device.
   switch (offset) {
     case 0:
+      baudrate = value & 3;
       break;
     case 1:
+      device = static_cast<Device>(value & 3);
+      bugged_hword_mode = value & 4;
+      chipselect_hold = value & 8;
+      enable_irq = value & 64;
+      enable = value & 128;
       break;
     default:
       UNREACHABLE;
@@ -45,6 +56,8 @@ auto SPI::SPIDATA::ReadByte() -> u8 {
 }
 
 void SPI::SPIDATA::WriteByte(u8 value) {
+  if (spi.spicnt.enable && spi.spicnt.device == Device::Touchscreen)
+    LOG_ERROR("accessing touchscreen!!!");
 }
 
 } // namespace fauxDS::core
