7cb511e296bd10a1852bc15d96eaf7784fdc2712
fleroviux <fleroviux@gmail.com>
2021-02-07 22:09:29 +0100

    Core: only skip to the next event if both CPUs are halted.

--- source/main.cpp
+++ source/main.cpp
@@ -107,10 +107,10 @@ void loop(ARM* arm7, ARM* arm9, Interconnect* interconnect, ARM7MemoryBus* arm7_
       // that we do not run past any hardware event.
       if (gLooselySynchronizeCPUs) {
         u64 target = std::min(frame_target, scheduler.GetTimestampTarget());
+        // Run to the next event if both CPUs are halted.
+        // Otherwise run each CPU for up to 32 cycles.
         cycles = target - scheduler.GetTimestampNow();
-
-        // Run to the next event if at most one CPU is running.
-        if (!arm9->IsWaitingForIRQ() && !arm7_mem->IsHalted()) {
+        if (!arm9->IsWaitingForIRQ() || !arm7_mem->IsHalted()) {
           cycles = std::min(32, cycles);
         }
       }
@@ -151,7 +151,7 @@ void loop(ARM* arm7, ARM* arm9, Interconnect* interconnect, ARM7MemoryBus* arm7_
     u32 const* output_top = interconnect->video_unit.GetOutput(VideoUnit::Screen::Top);
     u32 const* output_bottom = interconnect->video_unit.GetOutput(VideoUnit::Screen::Bottom);
 
-    glClear(GL_COLOR_BUFFER_BIT);
+    /*glClear(GL_COLOR_BUFFER_BIT);
 
     glBindTexture(GL_TEXTURE_2D, textures[0]);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 256, 192, 0, GL_BGRA, GL_UNSIGNED_BYTE, output_top);
@@ -181,7 +181,7 @@ void loop(ARM* arm7, ARM* arm9, Interconnect* interconnect, ARM7MemoryBus* arm7_
     glVertex2f(-1.0f, -1.0f);
     glEnd();
 
-    SDL_GL_SwapWindow(window);
+    SDL_GL_SwapWindow(window);*/
 
     while (SDL_PollEvent(&event)) {
       if (event.type == SDL_QUIT)
