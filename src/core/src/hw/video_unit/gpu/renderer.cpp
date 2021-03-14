7c686a0e43483ba76e707e17ef1c712b4d8dc260
Frederic Meyer <fleroviux@gmail.com>
2021-03-14 18:09:12 +0100

    GPU: preliminary 4x4 compressed texture emulation.

--- src/core/src/hw/video_unit/gpu/renderer.cpp
+++ src/core/src/hw/video_unit/gpu/renderer.cpp
@@ -31,6 +31,7 @@ auto GPU::SampleTexture(TextureParams const& params, s16 u, s16 v) -> Color4 {
   }
 
   auto offset = coord[1] * size[0] + coord[0];
+  auto palette_addr = params.palette_base << 4;
 
   switch (params.format) {
     case TextureParams::Format::None: {
@@ -41,7 +42,7 @@ auto GPU::SampleTexture(TextureParams const& params, s16 u, s16 v) -> Color4 {
       int index = value & 0x1F;
       int alpha = value >> 5;
 
-      auto rgb555 = vram_palette.Read<u16>((params.palette_base << 4) + index * sizeof(u16)) & 0x7FFF;
+      auto rgb555 = vram_palette.Read<u16>(palette_addr + index * sizeof(u16)) & 0x7FFF;
       auto rgb9999 = Color4::from_rgb555(rgb555);
       
       if (params.color0_transparent && index == 0) {
@@ -62,7 +63,7 @@ auto GPU::SampleTexture(TextureParams const& params, s16 u, s16 v) -> Color4 {
         return kTransparentColor;
       }
       
-      return Color4::from_rgb555(vram_palette.Read<u16>((params.palette_base << 3) + index * sizeof(u16)) & 0x7FFF);
+      return Color4::from_rgb555(vram_palette.Read<u16>((palette_addr >> 1) + index * sizeof(u16)) & 0x7FFF);
     }
     case TextureParams::Format::Palette4BPP: {
       auto index = (vram_texture.Read<u8>(params.address + (offset >> 1)) >> (4 * (offset & 1))) & 15;
@@ -71,7 +72,7 @@ auto GPU::SampleTexture(TextureParams const& params, s16 u, s16 v) -> Color4 {
         return kTransparentColor;
       }
       
-      return Color4::from_rgb555(vram_palette.Read<u16>((params.palette_base << 4) + index * sizeof(u16)) & 0x7FFF);
+      return Color4::from_rgb555(vram_palette.Read<u16>(palette_addr + index * sizeof(u16)) & 0x7FFF);
     }
     case TextureParams::Format::Palette8BPP: {
       auto index = vram_texture.Read<u8>(params.address + offset);
@@ -80,7 +81,66 @@ auto GPU::SampleTexture(TextureParams const& params, s16 u, s16 v) -> Color4 {
         return kTransparentColor;
       }
 
-      return Color4::from_rgb555(vram_palette.Read<u16>((params.palette_base << 4) + index * sizeof(u16)) & 0x7FFF);
+      return Color4::from_rgb555(vram_palette.Read<u16>(palette_addr + index * sizeof(u16)) & 0x7FFF);
+    }
+    case TextureParams::Format::Compressed4x4: {
+      auto row_x = coord[0] >> 2;
+      auto row_y = coord[1] >> 2;
+      auto block = row_y * (size[0] >> 2) + row_x;
+
+      auto data = vram_texture.Read<u32>(params.address + block * sizeof(u32)); 
+      auto info = vram_texture.Read<u16>(((params.address & 0x1FFFF) | 0x20000) + ((params.address >> 18) << 16) + block * sizeof(u16));
+      auto palette_offset = info & 0x3FFF;
+      auto mode = info >> 14;
+
+      auto shift = ((coord[1] & 3) * 8 + (coord[0] & 3) * 2);
+      auto index = (data >> shift) & 3;
+
+      palette_addr += palette_offset << 2;
+
+      switch (mode) {
+        case 0: {
+          if (index == 3) {
+            return kTransparentColor;
+          }
+          return Color4::from_rgb555(vram_palette.Read<u16>(palette_addr + index * sizeof(u16)) & 0x7FFF);
+        }
+        case 1: {
+          if (index == 2) {
+            auto color_0 = Color4::from_rgb555(vram_palette.Read<u16>(palette_addr + 0) & 0x7FFF);
+            auto color_1 = Color4::from_rgb555(vram_palette.Read<u16>(palette_addr + 2) & 0x7FFF);
+
+            for (uint i = 0; i < 3; i++) {
+              color_0[i] = detail::ColorComponent{(color_0[i].raw() >> 1) + (color_1[i].raw() >> 1)};
+            }
+
+            return color_0;
+          }
+          if (index == 3) {
+            return kTransparentColor;
+          }
+          return Color4::from_rgb555(vram_palette.Read<u16>(palette_addr + index * sizeof(u16)) & 0x7FFF);
+        }
+        case 2: {
+          return Color4::from_rgb555(vram_palette.Read<u16>(palette_addr + index * sizeof(u16)) & 0x7FFF);
+        }
+        default: {
+          if (index == 2 || index == 3) {
+            int coeff_0 = index == 2 ? 5 : 3;
+            int coeff_1 = index == 2 ? 3 : 5;
+
+            auto color_0 = Color4::from_rgb555(vram_palette.Read<u16>(palette_addr + 0) & 0x7FFF);
+            auto color_1 = Color4::from_rgb555(vram_palette.Read<u16>(palette_addr + 2) & 0x7FFF);
+
+            for (uint i = 0; i < 3; i++) {
+              color_0[i] = detail::ColorComponent{((color_0[i].raw() * coeff_0) + (color_1[i].raw() * coeff_1)) >> 3};
+            }
+
+            return color_0;
+          }
+          return Color4::from_rgb555(vram_palette.Read<u16>(palette_addr + index * sizeof(u16)) & 0x7FFF);
+        }
+      }
     }
     case TextureParams::Format::A5I3: {
       u8  value = vram_texture.Read<u8>(params.address + offset);
